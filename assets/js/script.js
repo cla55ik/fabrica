55da97b835054c6c26141f4156780a6e268a3a3c
Ivan <ivan5420@yandex.ru>
2021-09-13 10:10:18 +0300

    add mobile carousel

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,22 +8,85 @@ window.addEventListener('DOMContentLoaded', function() {
     tabBtnSignUp.addEventListener('click', function() {
 
 
-        if (!tabBtnSignUp.classList.contains('active')) {
+        if (!tabBtnSignUp.classList.contains('active_tab')) {
             tabSignUp.classList.toggle('hidden');
             tabSignIn.classList.toggle('hidden');
-            tabBtnSignUp.classList.toggle('active');
-            tabBtnSignIn.classList.toggle('active');
+            tabBtnSignUp.classList.toggle('active_tab');
+            tabBtnSignIn.classList.toggle('active_tab');
         }
     })
 
 
     tabBtnSignIn.addEventListener('click', function() {
 
-        if (!tabBtnSignIn.classList.contains('active')) {
+        if (!tabBtnSignIn.classList.contains('active_tab')) {
             tabSignUp.classList.toggle('hidden');
             tabSignIn.classList.toggle('hidden');
-            tabBtnSignUp.classList.toggle('active');
-            tabBtnSignIn.classList.toggle('active');
+            tabBtnSignUp.classList.toggle('active_tab');
+            tabBtnSignIn.classList.toggle('active_tab');
         }
     })
+
+
+
+
+    $('#project_slider').owlCarousel({
+        loop: true,
+        margin: 10,
+        nav: false,
+        dots: false,
+        responsive: {
+            0: {
+                items: 1
+            },
+            600: {
+                items: 3
+            },
+            1000: {
+                items: 3
+            }
+        }
+    })
+
+    $('#feedback_carousel').owlCarousel({
+        loop: true,
+        margin: 10,
+        nav: false,
+        dots: false,
+        responsive: {
+            0: {
+                items: 1
+            },
+            600: {
+                items: 3
+            },
+            1000: {
+                items: 3
+            }
+        }
+    });
+
+
+    //Делегируем события кнопок next prev по умолчанию нашим кнопкам, которые могут находится ыне контейнера слайдера
+    var owl_fb = $("#feedback_carousel");
+    owl_fb.owlCarousel();
+    //$(".next") - находим нашу кнопку
+    $(".feedback_next_btn").click(function() {
+        owl_fb.trigger("next.owl.carousel");
+    });
+    $(".feedback_prev_btn").click(function() {
+        owl_fb.trigger("prev.owl.carousel");
+    });
+
+
+
+    var owl_proj = $("#project_slider");
+    owl_proj.owlCarousel();
+    //$(".next") - находим нашу кнопку
+    $(".proj_next_btn").click(function() {
+        owl_proj.trigger("next.owl.carousel");
+    });
+    $(".proj_prev_btn").click(function() {
+        owl_proj.trigger("prev.owl.carousel");
+    });
 })
